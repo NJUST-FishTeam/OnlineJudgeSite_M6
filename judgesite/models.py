6943795bf483126be282047fb21c7c6718b93421
maemual <maemual@gmail.com>
2015-04-04 16:58:27 +0800

    线上适配

--- judgesite/models.py
+++ judgesite/models.py
@@ -15,6 +15,7 @@ engine = create_engine("mysql://{0}:{1}@{2}/{3}?charset=utf8".format(conf.mysql_
 
 
 def save_result(status_id=0, type='normal', run_time=0, run_memory=0, compiler_output="", status="SystemError"):
+    conn = engine.connect()
     if type == 'normal':
         sql = text(
             '''
@@ -28,8 +29,9 @@ def save_result(status_id=0, type='normal', run_time=0, run_memory=0, compiler_o
             '''
         )
 
-    engine.execute(sql, compiler_output=compiler_output, run_time=run_time, run_memory=run_memory, status=status, status_id=status_id)
+    conn.execute(sql, compiler_output=compiler_output, run_time=run_time, run_memory=run_memory, status=status, status_id=status_id)
     update_counters(status_id, type)
+    conn.close()
 
 
 def update_counters(status_id, type):
--- judgesite/service.py
+++ judgesite/service.py
@@ -14,11 +14,11 @@ class JudgeSite(object):
     def __init__(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=conf.rmq_host, port=conf.rmq_port))
         self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=conf.rmq_queue, durable=True)
 
         self.channel.basic_consume(self._consume, queue=conf.rmq_queue, no_ack=True)
 
     def _consume(self, ch, method, properties, body):
-        #print " [x] Received %r" % (body,)
         logging.info("GOT A TASK!")
         task = JudgeTask(body)
         task.go()
